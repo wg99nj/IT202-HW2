0fd8b37e3e8b4e92dde067dd3214a08c8994731d
wg99nj <wg99@njit.edu>
2025-06-09 18:20:41 -0400

    solved problem 4 but this time with a middle character.

--- public_html/M2/problem4.php
+++ public_html/M2/problem4.php
@@ -2,9 +2,9 @@
 
 require_once "base.php";
 
-$ucid = "wg99"; // <-- set your UCID
+$ucid = "wg99"; // <-- set your ucid
 
-// Test arrays (do not edit)
+// Don't edit the arrays below, they are used to test your code
 $array1 = ["hello world!", "php programming", "special@#$%^&characters", "numbers 123 456", "mIxEd CaSe InPut!"];
 $array2 = ["hello world", "php programming", "this is a title case test", "capitalize every word", "mixEd CASE input"];
 $array3 = ["  hello   world  ", "php    programming  ", "  extra    spaces  between   words   ",
@@ -12,22 +12,28 @@ $array3 = ["  hello   world  ", "php    programming  ", "  extra    spaces  betw
 $array4 = ["hello world", "php programming", "short", "a", "even"];
 
 function transformText($arr, $arrayNumber) {
-    // Display original input
     printArrayInfoBasic($arr, $arrayNumber);
 
     foreach ($arr as $index => $text) {
         // Step 1: Remove non-alphanumeric characters except spaces
-        $clean = preg_replace("/[^a-zA-Z0-9 ]/", "", $text);
+        $text = preg_replace("/[^a-zA-Z0-9 ]/", "", $text);
 
-        // Step 2: Remove duplicate spaces and trim
-        $clean = preg_replace('/\s+/', ' ', $clean);
-        $clean = trim($clean);
+        // Step 2: Trim and reduce multiple spaces to one
+        $text = preg_replace("/\s+/", " ", trim($text));
 
         // Step 3: Convert to Title Case
-        $placeholderForModifiedPhrase = ucwords(strtolower($clean));
+        $placeholderForModifiedPhrase = ucwords(strtolower($text));
 
-        
-        $placeholderForMiddleCharacters = "";
+        // Step 4: Get middle 3 characters (not first/last)
+        $cleaned = str_replace(" ", "", $placeholderForModifiedPhrase); // no spaces for middle char calc
+        $length = strlen($cleaned);
+        if ($length < 3) {
+            $placeholderForMiddleCharacters = "Not enough characters";
+        } else {
+            $middle = intdiv($length, 2);
+            $start = max(1, $middle - 1); // avoid first char
+            $placeholderForMiddleCharacters = substr($cleaned, $start, 3);
+        }
 
         echo "<div>";
         printStringTransformations($index, $placeholderForModifiedPhrase, $placeholderForMiddleCharacters);
@@ -37,12 +43,11 @@ function transformText($arr, $arrayNumber) {
     echo "<br>______________________________________<br>";
 }
 
-// Run the function on all arrays
+// Run the problem
 printHeader($ucid, 4);
 transformText($array1, 1);
 transformText($array2, 2);
 transformText($array3, 3);
 transformText($array4, 4);
 printFooter($ucid, 4);
-
 ?>
